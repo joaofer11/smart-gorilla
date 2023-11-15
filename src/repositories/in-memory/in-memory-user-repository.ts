8b3115f378556141416ea9d68509fd750aaf5f01
João Fernandes <joaofergear003@gmail.com>
2023-11-15 14:31:33 -0300

    feat: create in-memory user repository

---
+++ src/repositories/in-memory/in-memory-user-repository.ts
@@ -0,0 +1,29 @@
+import { randomUUID } from 'node:crypto';
+import { User } from '@/models/user';
+import { CreateUserDTO, UserRepository } from '../interfaces/user-repository';
+
+export class InMemoryUserRepository implements UserRepository {
+  items: User[] = [];
+
+  async findById(id: string) {
+    return this.items.find((item) => item.id === id) ?? null;
+  }
+
+  async findByEmail(email: string) {
+    return this.items.find((item) => item.email === email) ?? null;
+  }
+
+  async create(data: CreateUserDTO) {
+    const user = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      age: data.age,
+      email: data.email,
+      password: data.password,
+    };
+
+    this.items.push(user);
+
+    return user;
+  }
+}
---
+++ src/repositories/interfaces/user-repository.ts
@@ -0,0 +1,15 @@
+import { User } from '@/models/user';
+
+export interface CreateUserDTO {
+  id?: string | null;
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+}
+
+export interface UserRepository {
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  create(data: CreateUserDTO): Promise<User>;
+}
